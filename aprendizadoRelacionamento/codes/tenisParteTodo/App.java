f8c3408544549831117926dd687cffd9ef5e63a0
DevPapito <pietroeziosuzano@gmail.com>
2026-01-08 00:43:12 -0300

    Add tenisCode

---
+++ aprendizadoRelacionamento/codes/tenisParteTodo/App.java
@@ -0,0 +1,11 @@
+class App {
+
+    public static void main(String[] args) {
+
+        Tenis tenis = new Tenis(new Palpet());
+        tenis.addCadarco(new Cadarco());
+        tenis.addCadarco(new Cadarco());
+
+    }
+
+}
---
+++ aprendizadoRelacionamento/codes/tenisParteTodo/Tenis.java
@@ -0,0 +1,18 @@
+public class Tenis {
+
+    private final Palpet palpet;
+    private List<Cadarco> cadarcos = new ArrayList<>();
+
+    public Tenis(Palpet palpet) {
+
+        this.palpet = palpet;
+
+    }
+
+    public addCadarco(Cadarco cadarco) {
+
+        this.cadarcos.add(cadarco);
+
+    }
+
+}
