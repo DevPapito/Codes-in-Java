b60237db395bd118474722e21ebfe46f73e4e4c3
DevPapito <pietroeziosuzano@gmail.com>
2026-01-22 15:28:51 -0300

    Learn final word key

---
+++ aprendizadoFinal/App.java
@@ -0,0 +1,10 @@
+class App {
+
+    public static void main(String[] args) {
+
+        System.out.println(Constantes.JDBC);
+        System.out.println(Constantes.DOMAIN);
+
+    }
+
+}
---
+++ aprendizadoFinal/Constantes.java
@@ -0,0 +1,6 @@
+public final class Constantes {
+
+    public static final String JDBC = "http/127.0.0.1:5853";
+    public static final String DOMAIN = "App";
+
+}
